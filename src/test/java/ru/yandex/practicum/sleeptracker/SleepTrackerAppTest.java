2cdb98b7af9ce2f96a3db9363352759f54548f78
AlexandrNovizov <126908706+AlexandrNovizov@users.noreply.github.com>
2025-12-25 14:16:13 +0400

    Добавлен тест для выброса исключения из SleepingSession::nightType

--- src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.sleeptracker.exceptions.IllegalTimeRangeException;
 import ru.yandex.practicum.sleeptracker.exceptions.InvalidStringFormatException;
+import ru.yandex.practicum.sleeptracker.exceptions.NotNightSessionException;
 
 import java.time.LocalDateTime;
 import java.time.Month;
@@ -18,11 +19,22 @@ public class SleepTrackerAppTest {
     }
 
     @Test
-    void shouldReturnIllegalTimeRangeExceptionForStartNotBeforeEnd() {
+    void shouldThrowIllegalTimeRangeExceptionForStartNotBeforeEnd() {
         assertThrows(IllegalTimeRangeException.class, () -> new SleepingSession(
                 LocalDateTime.of(2025, Month.OCTOBER, 1, 0, 0),
                 LocalDateTime.of(2025, Month.JUNE, 1, 0, 0),
                 SleepQuality.GOOD
         ));
+    }
+
+    @Test
+    void shouldThrowNotNightSessionExceptionForNotNightSession() {
+        SleepingSession session = new SleepingSession(
+                LocalDateTime.of(2025, Month.OCTOBER, 1, 13, 13),
+                LocalDateTime.of(2025, Month.OCTOBER, 1, 14, 0),
+                SleepQuality.NORMAL
+        );
+
+        assertThrows(NotNightSessionException.class, session::nightType);
     }
 }
